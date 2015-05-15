4f5b34e1597176ae74a8d19de9b1070abbc887d7
Barry Oliver <i31770@gmail.com>
2015-05-15 11:46:39 -0400

    grid works, button not working correctly

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-    for (var x = 0; x < 16; x++) {
-        $('.grid').append('<div class="blocks">(div)</div>').css("border","yellow")
-    }
+    var displaySize = 960;
+    var gridSize = 100;
+    var grid = gridSize*gridSize;
+    var blockSize = displaySize/gridSize;
+
+
+
+    for (var x = 0; x < grid; x++) {
+        $('.inner').append('<div class="blocks"></div>');}
+         $('.blocks').css({'display':'inline-block','float':'left','width':'20px','height':'20px','outline':'1px solid'});
+
+    $('.blocks')
+        .mouseenter('div',function(){
+            $(this).css('background-color','black')
+        })
+        .mouseleave('div',function(){
+            $(this).css('background-color','white')
+        })
+    $('.clearButton').click(function(){
+        var clear = prompt("What size grid?");
+        if(clear != null){
+            for (var x = 0; x < grid; x++) {
+                $('.inner').removeClass('.blocks');
+                gridSize = clear;
+            }
+
+        }
+})
+
 });
